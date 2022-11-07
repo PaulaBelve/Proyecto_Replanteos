aefebe753782eda28a026faffaca4b48401568af
Paula Belvedere <pau.belve@gmail.com>
2022-11-07 15:16:52 -0300

    correcionesort

--- JavaScript/objetos.js
+++ JavaScript/objetos.js
@@ -56,7 +56,7 @@ const equipo = [
     profesion: 'Lic. Julieta Aued Santi | Psicóloga ',
     universidad: 'Egresada de la UBA',
     presentacion: 'Realizó la Residencia en Psicología Clínica en el Hospital General de Agudos “Parmenio T. Piñero”, con una formación clínica especializada de orientación psicoanalítica. Se formó como Acompañante Terapéutico en el CPsA (Centro Psicosocial Argentino)...',
-    subtitulo: 'Julieta, forma parte del equipo de Replanteos, es Lic. en Psicólogia, egresada de la Universidad Nacional de Buenos Aires.',
+    subtitulo: 'Julieta, forma parte del equipo de Replanteos, es Lic. en Psicología, egresada de la Universidad Nacional de Buenos Aires.',
     estudios: 'Realizó la Residencia en Psicología Clínica en el Hospital General de Agudos “Parmenio T. Piñero”, con una formación clínica especializada de orientación psicoanalítica. Se formó como Acompañante Terapéutico en el CPsA (Centro Psicosocial Argentino).',
     CV: 'Posee amplia experiencia clínica en la atención de pacientes con Trastornos Mentales Severos en diversos dispositivos: internación, consultorios externos, guardia interna y externa, interconsulta y hospital de día. Se desempeña como psicóloga de planta en el Área Programática del Hospital General de Agudos “Parmenio T. Piñero” y como psicóloga de guardia en una Institución Psicoterapéutica dedicada a la recuperación y la inclusión social. También realiza atención clínica de orientación psicoanalítica a adolescentes y adultos, y es docente de posgrado en “Residir en Psico”: escuela y espacio de orientación para jóvenes graduados de Psicología en la preparación integral para rendir el Examen de Residencia de Psicología Clínica.',
     img: '../IMG/JULI.png'
@@ -81,7 +81,7 @@ const equipo = [
     profesion: 'Lic. Paula Fernandez | Psicóloga ',
     universidad: 'Egresada de la UBA',
     presentacion: 'Psicoanalista ICDEBA. Integra el equipo de adicciones del municipio de Vicente López, donde se desempeña como psicóloga clínica. Formó parte del dispositivo de atención del Colegio de psicólogos XV... ',
-    subtitulo: 'Paula, forma parte del equipo de Replanteos, es Lic. en Psicólogia, egresada de la Universidad Nacional de Buenos Aires.',
+    subtitulo: 'Paula, forma parte del equipo de Replanteos, es Lic. en Psicología, egresada de la Universidad Nacional de Buenos Aires.',
     estudios: 'Psicoanalista ICDEBA',
     CV: 'Integra el equipo de adicciones del municipio de Vicente López, donde se desempeña como psicóloga clínica. Se desempeña como docente en la cátedra de Anorexia Bulimia y Obesidad en la  UBA. Formó parte del dispositivo de atención del colegio de psicólogos XV. Trabajó en Cepia Salud Mental, hospital de día. Trabajó en la guardia de Salud mental del Hospital Houssay. Su experiencia clínica se basa en patologías graves, apostando al trabajo en equipo tanto en el ámbito público como privado. ',
     img: '../IMG/PAU1.png'
@@ -92,9 +92,9 @@ const equipo = [
     nombre: 'Raquel Maggi',
     profesion: 'Lic. Raquel Maggi | Psicoanalista ',
     universidad: 'Egresada de la UBA',
-    presentacion: 'Realizó su residencia en la clínica Hospital municipal de San Isidro “Dr. M. Posse”. Se especializó como acompañante terapéutico/Centro de salud mental 3 Dr. A. Ameghino. Es miembro del equipo de Salud mental atención de adultos...',
-    subtitulo: 'Raquel, forma parte del equipo de Replanteos, es Lic. en Psicólogia, egresada de la Universidad Nacional de Buenos Aires.',
-    estudios: 'Se formo como Acompañante terapéutico/Centro de salud mental 3 Dr. A. Ameghino (1993), realizó su residencia clínica en el Hospital municipal de San Isidro “Dr. M. Posse” (1996/2003).',
+    presentacion: 'Realizó su residencia clínica en el Hospital Municipal de San Isidro “Dr. M. Posse”. Se especializó como acompañante terapéutico/Centro de salud mental 3 Dr. A. Ameghino. Es miembro del equipo de Salud mental atención de adultos...',
+    subtitulo: 'Raquel, forma parte del equipo de Replanteos, es Lic. en Psicología, egresada de la Universidad Nacional de Buenos Aires.',
+    estudios: 'Se formo como Acompañante terapéutico/Centro de Salud Mental n°3 Dr. A. Ameghino (1993), realizó su residencia clínica en el Hospital Municipal de San Isidro “Dr. M. Posse” (1996/2003).',
     CV: 'Miembro del equipo de Salud mental atención de adultos. Asistencia psicológica en consultorios externos, sala de internación, y guardia. Coordinadora de talleres del Centro de día. Coolaboradora docente en pasantías universitarias (UBA). Miembro del staff de profesionales de Swiss Medical group (2003/2013). Atención psicológica de pacientes adolescentes y adultos en consultorio. Miembro del equipo de admisiones. Miembro del staff de profesionales de Medicus (2008/2014). Atención psicológica en consultorio de pacientes adultos. Miembro del equipo de cuidados paliativos en domicilio. Miembro del staff de profesionales de Osde (2016/a la fecha). Atención en consultorio de pacientes adultos. Práctica privada (1996/a la fecha). Atención de pacientes adolescentes, adultos, y parejas, Consultorio particular en zona norte del GBA, modalidad presencial y virtual.',
     img: '../IMG/RAQUEL.png'
   },
